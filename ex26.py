42800ec697daec23fb306a6bcaeaca2d8b4399f0
Alex Song <31237906+TheRealAlexSong@users.noreply.github.com>
2018-11-04 22:54:45 -0800

    Add files via upload

--- ex26.py
+++ ex26.py
@@ -1,29 +1,32 @@
+from sys import argv # added import of argv
+
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
-print("How much do you weigh?", end=' '
+height = input() # added variable and input
+print("How much do you weigh?", end=' ') # added closing bracket
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 script, filename = argv
 
-txt = open(filenme)
+txt = open(filename) # corrected spelling of 'filename'
 
 print("Here's your file {filename}:")
-print(tx.read())
+print(txt.read()) # corrected spelling of 'txt'
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read()) # corrected parameter (or is it argument?)
 
 
-print('Let's practice everything.')
-print('You\'d need to know \'bout escapes
-      with \\ that do \n newlines and \t tabs.')
+print('Let\'s practice everything.')
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.') # split into two lines for formatting
 
 poem = """
 \tThe lovely world
@@ -34,23 +37,23 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print("--------------)
+print("--------------") # correctly enclosed in quotes
 print(poem)
-print(--------------")
+print("--------------") # correctly enclosed in quotes
 
 
-five = 10 - 2 + 3 -
-print(f"This should be five: {five}"
+five = 10 - 2 + 3 - 6 # added '6' to correct the issue
+print(f"This should be five: {five}") # added closing parentheses
 
-def secret_formula(started)
+def secret_formula(started): # added colon
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars / 100 # added '/' operator
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point) # added 'crates' argument
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
@@ -60,27 +63,27 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point) # added underscore to start_point
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-cates = 30
+cats = 30 # corrected spelling of 'cats'
 dogs = 15
 
 
 if people < cats:
-    print "Too many cats! The world is doomed!"
+    print ("Too many cats! The world is doomed!") # added parentheses
 
-if people < cats:
+if people > cats: # corrected operator from less than to greater than
     print("Not many cats! The world is saved!")
 
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs
+if people > dogs: # added colon
     print("The world is dry!")
 
 
@@ -89,9 +92,9 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
-    print("People are less than or equal to dogs.)
+if people <= dogs: # added colon
+    print("People are less than or equal to dogs.") # added final quote
 
 
-if people = dogs:
+if people == dogs: # changed from '=' to '==' for operator comparison operator
     print("People are dogs.")
